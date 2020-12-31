64f38f9690d6654f125463dd54df00162f75978a
braeden <braedensmith2@gmail.com>
2020-12-30 22:06:28 -0600

    abstract swap (should be done)

--- heap.js
+++ heap.js
@@ -1,12 +1,11 @@
+// default constructor is min-heap
 class Heap {
-    // default constructor is min-heap
     constructor(comparator = (a, b) => a - b, input = []) {
         this.arr = input;
         this.comparator = comparator;
         if (this.size() > 1) {
-            for (let i = Heap.parent(this.size() - 1); i >= 0; i--) {
+            for (let i = Heap.parent(this.size() - 1); i >= 0; i--)
                 this.heapifyDown(i);
-            }
         }
     }
 
@@ -22,11 +21,7 @@ class Heap {
     pop() {
         if (this.size() === 0)
             return null;
-
-        const temp = this.arr[0];
-        this.arr[0] = this.arr[this.size() - 1];
-        this.arr[this.size() - 1] = temp;
-
+        Heap.swap(this.arr, 0, this.size() - 1);
         const result = this.arr.pop();
         this.heapifyDown(0);
         return result;
@@ -47,9 +42,7 @@ class Heap {
             return;
         const parentIdx = Heap.parent(idx);
         if (this.comparator(this.arr[idx], this.arr[parentIdx]) < 0) {
-            const temp = this.arr[parentIdx];
-            this.arr[parentIdx] = this.arr[idx];
-            this.arr[idx] = temp;
+            Heap.swap(this.arr, parentIdx, idx);
             this.heapifyUp(parentIdx);
         }
     }
@@ -59,13 +52,16 @@ class Heap {
             return;
         const childIdx = this.maxPriorityChild(idx);
         if (this.comparator(this.arr[childIdx], this.arr[idx]) < 0) {
-            const temp = this.arr[childIdx];
-            this.arr[childIdx] = this.arr[idx];
-            this.arr[idx] = temp;
+            Heap.swap(this.arr, childIdx, idx);
             this.heapifyDown(childIdx);
         }
     }
 
+    static swap(arr, idxOne, idxTwo) {
+        const temp = arr[idxOne];
+        arr[idxOne] = arr[idxTwo];
+        arr[idxTwo] = temp;
+    }
     static leftChild(idx) { return 2 * idx + 1; }
     static rightChild(idx) { return 2 * idx + 2; };
     static parent(idx) { return Math.floor((idx - 1) / 2); }
@@ -74,7 +70,7 @@ class Heap {
 
 ///////////////////////////////////////
 
-const h = new Heap((a, b) => a - b);
+const h = new Heap((a, b) => b - a);
 for (let i = 10; i > -10; i--) {
     h.push(i);
 }
